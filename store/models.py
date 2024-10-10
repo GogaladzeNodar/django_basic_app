e28ca68b7f013b8594f288b27e26b36af4524140
Nodara <nodo.gogaladze2002@gmail.com>
2024-10-10 11:10:47 +0400

    add Image field for product Model and define Root Directory for images

--- store/models.py
+++ store/models.py
@@ -7,9 +7,13 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=False, blank=False)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to="images/products/")
     stock = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
+
+
+class Category(models.Model):
